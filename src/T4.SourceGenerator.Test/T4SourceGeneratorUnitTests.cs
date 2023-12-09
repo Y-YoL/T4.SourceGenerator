5bf25249f12b623139061e917ede086f7d345589
y-sasaki <11359025+Y-YoL@users.noreply.github.com>
2023-12-09 17:53:34 +0900

    Add project

---
+++ src/T4.SourceGenerator.Test/T4SourceGeneratorUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = T4.SourceGenerator.Test.CSharpCodeFixVerifier<
+    T4.SourceGenerator.T4SourceGeneratorAnalyzer,
+    T4.SourceGenerator.T4SourceGeneratorCodeFixProvider>;
+
+namespace T4.SourceGenerator.Test
+{
+    [TestClass]
+    public class T4SourceGeneratorUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("T4SourceGenerator").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
