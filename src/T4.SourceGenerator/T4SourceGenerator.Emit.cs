5f8d1b28077498fe2016d83fa15571bde1135aca
y-sasaki <11359025+Y-YoL@users.noreply.github.com>
2023-12-10 17:08:37 +0900

    Add Mono.TextTemplating

---
+++ src/T4.SourceGenerator/T4SourceGenerator.Emit.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using Mono.TextTemplating;
+
+namespace T4.SourceGenerator;
+
+public partial class T4SourceGenerator : IIncrementalGenerator
+{
+    private static void Generate(SourceProductionContext context, AdditionalText source)
+    {
+        string filename = string.Empty;
+
+        var generator = new TemplateGenerator();
+        generator.ProcessTemplate(
+            source.Path,
+            source.GetText()?.ToString(),
+            ref filename,
+            out var content);
+
+        context.AddSource(filename, content);
+    }
+}
--- src/T4.SourceGenerator/T4SourceGenerator.cs
+++ src/T4.SourceGenerator/T4SourceGenerator.cs
@@ -2,10 +2,33 @@
 
 namespace T4.SourceGenerator;
 
-public class T4SourceGenerator : IIncrementalGenerator
+public partial class T4SourceGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        throw new System.NotImplementedException();
+        var source = context.AdditionalTextsProvider
+            .Where(static file => file.Path.EndsWith(".tt", StringComparison.InvariantCultureIgnoreCase))
+            .WithComparer(Comparer.Instance);
+
+        context.RegisterSourceOutput(source, static (context, source) =>
+        {
+            Generate(context, source);
+        });
+    }
+
+    private class Comparer : IEqualityComparer<AdditionalText>
+    {
+        public static readonly Comparer Instance = new();
+
+        public bool Equals(AdditionalText x, AdditionalText y)
+        {
+            return (X: x.GetText(), Y: y.GetText()) switch
+            {
+                { X: { } text1, Y: { } text2 } => text1.GetChecksum().SequenceEqual(text2.GetChecksum()),
+                _ => false,
+            };
+        }
+
+        public int GetHashCode(AdditionalText obj) => obj.GetHashCode();
     }
 }
